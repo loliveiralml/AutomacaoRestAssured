ce967bab5397d26295b9d93f7622c85b413995f0
Leandro <loliveiralml@gmail.com>
2024-02-26 15:03:51 -0300

    Inclusão de novos testes para atender um CRUD minimo.

---
+++ src/test/java/payload.java
@@ -0,0 +1,13 @@
+public class payload {
+
+    public static String loadCreateUser() {
+        return "{" + " \"name\": \"LeandroLML\"," + " \"job\": \"Engennier IT\"" + "}";
+    }
+
+    public static String updateUser() {
+        return "{" + " \"name\": \"LeandroLMLx\"," + " \"job\": \"Engennier Senior IT\"" + "}";
+    }
+    public static String loginUser() {
+        return "{" + " \"email\": \"peter@klaven\"}";
+    }
+}
---
+++ src/test/java/payloadOut.java
@@ -0,0 +1,22 @@
+public class payloadOut {
+    public static String errorReponseLoginUser() {
+        return "Missing password";
+    }
+    public static String ReponseUpdateUser() {
+        return "LeandroLMLx";
+    }
+
+    public static String ReponseUpdateJob() {
+
+        return "Engennier Senior IT";
+    }
+    public static String ReponsejobCreateUser() {
+
+        return "Engennier IT";
+    }
+
+    public static String ReponseNameCreateUser() {
+
+        return "LeandroLML";
+    }
+}
